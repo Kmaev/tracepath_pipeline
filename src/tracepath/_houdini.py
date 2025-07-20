37e49fb3e921af0ba9c5113b9557ac812a3a4530
Kristina Maevskaya <maevskayakrisfx@gmail.com>
2025-07-21 07:10:08 +1000

    Add prototype for HDA "USD Write"; update folder structure
    Simplify the process of loading and stitching sequence caches

--- src/tracepath/_houdini.py
+++ src/tracepath/_houdini.py
@@ -1,11 +1,16 @@
 import json
 import os
 import re
+from importlib import reload
 from pathlib import Path
+from tracepath import _usd
+
+reload(_usd)
 
 import hou
 
 
+# Load HDA
 def get_env() -> dict:
     """
     Gets environment variables for path resolution.
@@ -48,7 +53,7 @@ def get_manifest_context(node: hou.Node, templ) -> str:
     node_vars = get_node_env_data(node)
     all_node_data = {**env_vars, **node_vars}
 
-    templ_folder,_ = get_path_structure_templ(templ)
+    templ_folder, _ = get_path_structure_templ(templ)
     context = templ_folder.format(**all_node_data)
 
     return context
@@ -60,6 +65,7 @@ def set_latest_version(node: hou.Node, context: str):
     """
     version = get_latest_version_number(str(context))
     node.parm("version").set(version)
+
 
 
 def load_shot_manifest(node: hou.Node) -> str:
@@ -112,3 +118,78 @@ def get_latest_version_number(context: str) -> int | None:
         version = sorted(versioned_dirs, reverse=True)[0]
         return version
     return None
+
+
+# Write HDA:
+
+def get_usd_output_path(node: hou.Node, template) -> str:
+    """
+    Solves the usd output file path using environment variables and the selected template.
+    """
+    env_vars = get_env()
+    node_vars = get_node_env_data(node)
+
+    node_vars["name"] = node.parm("name").eval()
+    node_vars["version"] = str(node.parm("version").eval()).zfill(3)
+    node_vars["file_format"] = node.parm("format").evalAsString()
+    node_vars["padding"] = ".$F4" if node.evalParm("trange") else ""
+    all_node_data = {**env_vars, **node_vars}
+
+    # Resolving template
+    templ = get_path_structure_templ(template)
+    # Getting the first part of the output path string
+    output_path = templ.format(**all_node_data)
+
+    return output_path
+
+
+def get_first_frame_cache(node: hou.Node)->float:
+    """
+    Gets first frame cache for a given node.
+    """
+    first_frame = node.parm("f1").eval()
+    cache_parm = node.parm("lopoutput").evalAtFrame(first_frame)
+    return cache_parm
+
+
+def apply_autoversion(node: hou.Node):
+    """
+    This function called from HDA to version up the file
+    """
+    if node.parm("autoversion").eval() == 1:
+        context = Path(node.parm("lopoutput").evalAsString()).parent.parent
+        latest_version = get_latest_version_number(str(context))
+        if not latest_version:
+            latest_version = 1
+        later_version = str(latest_version + 1).zfill(3)
+        node.parm("version").set(later_version)
+
+
+def version_up_main_shot_manifest(node: hou.Node) -> str | None:
+    """
+    Versions up the main shot manifest path.
+    """
+    context = get_manifest_context(node, "usd_shot_manifest_output")
+    latest_version = get_latest_version_number(str(context))
+
+    output_path = find_matching_files(str(context), latest_version)
+    path = Path(output_path)
+    parent_folder = path.parent
+    file_name = path.name
+
+    match = re.search(r"(\d+)$", parent_folder.name)
+    if not match:
+        return None
+
+    version = match.group(1)
+    version_up= int(version) + (1 if hou.frame() == node.parm("f1").eval() else 0)
+
+    new_version = str(version_up).zfill(len(version))
+
+    new_folder_name = parent_folder.name.replace(version, new_version)
+    new_folder = parent_folder.parent / new_folder_name
+
+    new_file_name = file_name.replace(version, new_version)
+
+    new_output_path = new_folder / new_file_name
+    return str(new_output_path)
