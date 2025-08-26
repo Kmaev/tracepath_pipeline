482501d5241d1bd2c1c8d05269d385958df3680d
Kristina Maevskaya <maevskayakrisfx@gmail.com>
2025-08-27 06:28:51 +1000

    Separate ProjectIndex UI into different rez environment
    Tracepath terminal cleanup

--- setup/project_index.sh
+++ setup/project_index.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PR_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
@@ -9,16 +8,16 @@ echo "Set PR_ROOT to $PR_ROOT"
 
 if [ -z "$PR_PROJECTS_PATH" ]; then
     echo "[ERROR] PR_PROJECTS_PATH not set. Please export it before running this script."
-    return 1 2>/dev/null || exit 1
+    exit 1
 else
     echo "Using PR_PROJECTS_PATH: $PR_PROJECTS_PATH"
 fi
 
-export REZ_USED_RESOLVE_ENV=PR_PROJECTS_PATH
+export REZ_USED_RESOLVE_ENV="$PR_PROJECTS_PATH"
 
-rez env tracepath_terminal
+rez env project_index usd -- trace_project
 
-# Detect OS and close terminal after closing the application
+# Detect OS 
 case "$(uname)" in
   Darwin)  # macOS
     osascript -e 'tell application "Terminal" to close front window' & exit
@@ -31,3 +30,5 @@ case "$(uname)" in
     echo "Unsupported OS: $(uname)"
     ;;
 esac
+
+
--- setup/tracepath_terminal/show_nav/show_nav.sh
+++ setup/tracepath_terminal/show_nav/show_nav.sh
@@ -1,12 +1,13 @@
+GREEN='\033[38;5;34m'
+RESET='\033[0m'  # reset back to normal
+
 TASK_CREATE_SH="$TRACEPATH_SHELL_ROOT/task_create.sh"
 SUBFOLDERS_SH="$TRACEPATH_SHELL_ROOT/subfolders_create.sh"
-
-echo "[TRACEPATH] show_nav.sh sourced successfully"
 
 load() {
     export PR_SHOW="$1"
     export PR_SHOW_ROOT="$PR_PROJECTS_PATH/$PR_SHOW"
-    echo "[DEBUG] PR_SHOW=$PR_SHOW"
+    echo -e "${GREEN}[INFO] Active project: $PR_SHOW${RESET}"
 }
 
 cdtask() {
@@ -21,9 +22,10 @@ cdtask() {
 
             return 1
         }
-        echo "[INFO] Moved to: $path"
+        show_context
+        #echo "[INFO] Moved to: $path"
     else
-        echo "[ERROR] Path does not exist: $path"
+        echo "[ERROR] Task does not exist: $path"
         echo "========================================================================="
         "$TASK_CREATE_SH" "$PR_TASK" "$@"
         cd "$path" || return 1
@@ -33,7 +35,7 @@ cdtask() {
 add() {
     local path="$PR_SHOW_ROOT/$PR_GROUP/$PR_ITEM/$PR_TASK"
     if [[ ! -d "$path" ]]; then
-        echo "[ERROR] Path does not exist: $path"
+        echo "[ERROR] Context does not exist: $path"
         return 1
     fi
     cd "$path" || {
@@ -43,7 +45,16 @@ add() {
     cd "$path" || return 1
     "$SUBFOLDERS_SH" "$@"
     }
+
+show_context() {
+    local show=${PR_SHOW:-None}
+    local group=${PR_GROUP:-None}
+    local item=${PR_ITEM:-None}
+    local task=${PR_TASK:-None}
+
+    echo -e "${GREEN}[INFO] Project: $show${RESET} || ${GREEN}Group: $group${RESET} || ${GREEN}Item: $item${RESET} || ${GREEN}Task: $task${RESET}"
 }
+
 
 
 
