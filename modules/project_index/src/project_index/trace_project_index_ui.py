cb7691894ccd6ff2addd61e8fbd8975ead42c368
Kristina <160610175+Kmaev@users.noreply.github.com>
2025-12-28 12:05:34 +1100

    Fix silent error when Local Asset Library definition config is missing (#12)
    * Fix silent error when Local Asset Library definition config is missing
    
    Add an empty dictionary return on read_local_asset_lib_data if no file is found.
    Add an error on loading if the local asset library file is corrupted.

--- modules/project_index/src/project_index/trace_project_index_ui.py
+++ modules/project_index/src/project_index/trace_project_index_ui.py
@@ -348,8 +348,9 @@ class TraceProjectIndex(QtWidgets.QMainWindow):
                 self.tree_widget.takeTopLevelItem(index)
 
     def read_local_asset_lib_data(self) -> dict:
-        if not self.local_asset_lib:
-            logging.warning("Local Asset Library definition file not found in show data.")
+        if not os.path.isfile(self.local_asset_lib):
+            logging.warning("Local Asset Library definition file not found in config.")
+            return {}
         try:
             with open(self.local_asset_lib, "r") as f:
                 asset_lib_load = json.load(f)
@@ -358,8 +359,7 @@ class TraceProjectIndex(QtWidgets.QMainWindow):
             QtWidgets.QMessageBox.critical(
                 self,
                 "Error",
-                f"The asset_lib_data file at {self.local_asset_lib} is corrupted "
-                "and could not be read."
+                f"The asset_lib_data file at {self.local_asset_lib} is corrupted"
             )
             return {}
 
